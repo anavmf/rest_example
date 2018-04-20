0913e84e6c77c57beae5d1cbcde28e975a44297d
anavmferro <37638407+anavmferro@users.noreply.github.com>
2018-04-20 16:26:12 +0100

    Add files via upload
    added retrieval of the mobile phone list from the bucket

--- Mobile.php
+++ Mobile.php
@@ -2,18 +2,20 @@
 /* 
 A domain Class to demonstrate RESTful web services
 */
+require 'vendor/autoload.php';
+use Google\Cloud\Storage\StorageClient;
+
 Class Mobile {
-	
-	private $mobiles = array(
-		1 => 'Apple iPhone 6S',  
-		2 => 'Samsung Galaxy S6',  
-		3 => 'Apple iPhone 6S Plus',  			
-		4 => 'LG G4',  			
-		5 => 'Samsung Galaxy S6 edge',  
-		6 => 'OnePlus 2');
 		
 	public function getAllMobile(){
-		return $this->mobiles;
+		
+		$storage = new StorageClient();
+
+		$bucket = $storage->bucket('fcul-cc02-bucket');
+		$object = $bucket->object('list_mobiles.txt');
+
+		$string = $object->downloadAsString();
+		return json_decode($string);
 	}
 }
 ?>
